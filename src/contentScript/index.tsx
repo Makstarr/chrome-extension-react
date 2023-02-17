537d46710e2474ed27d13b56de37cbff3030b025
maks <maksi112@icloud.com>
2023-02-17 18:05:15 +0100

    Content script DOM

---
+++ src/contentScript/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+import '../assets/tailwind.css'
+import ContentScript from "./contentScript";
+
+function init() {
+    const appContainer = document.createElement('div')
+    appContainer.id = "react-boilerplate-root"
+    document.body.appendChild(appContainer)
+
+    const root = createRoot(appContainer)
+    root.render(<ContentScript/>);
+}
+
+init();
