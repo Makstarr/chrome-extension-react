75cdc209d4b8340d7ce789a9158d10ab2b0fbb57
maks <maksi112@icloud.com>
2023-02-20 17:37:00 +0100

    IDEA bugfix

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# ReactJS Chrome Extension
+
+## Stack
+- React
+- Typescript 
+- Webpack
+- Styled components
+
+## Extension components
+- Popup 
+- Settings page
+- Content injection
+
+## Basic functionality
+- Show button when text selected
+- Show modal when button is clicked 
---
+++ src/contentScript/contentScript.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+import { Button } from 'chrome-react-boilerplate/src/contentScript/components/button';
+import { Modal } from 'chrome-react-boilerplate/src/contentScript/components/modal';
+
+export default () => {
+  const [selectedText, setSelectedText] = useState('');
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  useEffect(() => {
+    const handleMouseUp = (event: MouseEvent) => {
+      if ((event.target as Element).id !== 'my-button') {
+        const text = window?.getSelection()?.toString() ?? '';
+        setSelectedText(text);
+      }
+    };
+
+    window.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, []);
+
+  return (
+    <>
+      {selectedText && (
+        <Button
+          id={'my-button'}
+          onClick={() => {
+            setIsModalVisible(true);
+          }}
+        />
+      )}
+      {isModalVisible && (
+        <Modal
+          selectedText={selectedText}
+          onClose={() => {
+            setIsModalVisible(false);
+          }}
+        />
+      )}
+    </>
+  );
+};
