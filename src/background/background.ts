ae77f36616313fc378e60bce7e99f64fda025b79
maks <maksi112@icloud.com>
2023-02-21 14:40:45 +0100

    Bugfix, styled-components Mozilla version

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,13 +1,12 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  sendResponse('Front the background Script');
-});
-
 chrome.contextMenus.create({
   id: 'react-boilerplate-extension',
   title: 'React Boilerplate context menu',
   contexts: ['selection'],
 });
 
-chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  await chrome.tabs.sendMessage(tab?.id ?? 0, info.selectionText);
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0]?.id ?? 0, info.selectionText );
+  });
 });
